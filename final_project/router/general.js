030715d689af169a099ff38e2238232169d9dcef
Daniel Friedrich <daniel.friedrich@slideandscroll.com>
2025-10-13 17:31:53 -0400

    Added code for tasks 1 and 2.

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -10,18 +10,26 @@ public_users.post("/register", (req,res) => {
   return res.status(300).json({message: "Yet to be implemented"});
 });
 
+
+
+
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    res.send(JSON.stringify(books,null,2));
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const isbn = req.params.isbn
+
+    res.send(JSON.stringify(books[isbn],null,2));
  });
   
+
+
+
+
+
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
